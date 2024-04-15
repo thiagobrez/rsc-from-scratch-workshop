93026f6a0d037eb18ab741724c91ab9383a6f6ca
Thiago Brezinski <thiagobrez@gmail.com>
2024-04-15 11:44:23 +0100

    client-side navigation, rehydration, async and client components

--- src/app/pokemon.jsx
+++ src/app/pokemon.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import Favorite from "./favorite.jsx";
 
-export default function Pokemon({ query }) {
+export default async function Pokemon({ query }) {
+    const data = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${query.name}`
+    ).then((res) => res.json());
+
   return (
-    <div>
-      <a href="/">Home</a>
-      <h1>
-        {query.name}
-      </h1>
-    </div>
+      <div>
+          <a href="/">Home</a>
+          <h1>
+              {query.name} ({data.types.map((type) => type.type.name).join(', ')})
+          </h1>
+          <div>
+              <Favorite name={query.name}/>
+          </div>
+          <img src={data.sprites.front_default}/>
+      </div>
   );
 }
--- src/server.jsx
+++ src/server.jsx
@@ -1,14 +1,32 @@
 import Router from '@koa/router';
+import { transform } from 'esbuild';
 import Koa from 'koa';
-import { createReadStream } from 'node:fs';
-import { stat } from 'node:fs/promises';
+import { AsyncLocalStorage } from 'node:async_hooks';
+import { readFile, readdir, stat } from 'node:fs/promises';
+import { register } from 'node:module';
 import { join } from 'node:path';
 import { renderToString } from 'react-dom/server';
+
+register('./loader.js', import.meta.url);
+
+const html = String.raw;
 
 const PORT = 5172;
 
 const app = new Koa();
 const router = new Router();
+
+router.get('/(.*).(js|jsx)', async (ctx) => {
+  const content = await readFile(join(import.meta.dirname, ctx.path), 'utf8');
+  const transformed = await transform(content, {
+    loader: 'jsx',
+    format: 'esm',
+    target: 'es2020',
+  });
+
+  ctx.type = 'text/javascript';
+  ctx.body = transformed.code;
+});
 
 router.get('/:path*', async (ctx) => {
   const filepath = `./app/${ctx.params.path ?? 'index'}.jsx`;
@@ -21,14 +39,12 @@ router.get('/:path*', async (ctx) => {
   const { default: Document } = await import('./app/_document.jsx');
   const { default: Page } = await import(filepath);
 
-  const html = renderToString(
+  await respond(
+    ctx,
     <Document>
       <Page query={ctx.request.query} />
     </Document>
   );
-
-  ctx.type = 'text/html';
-  ctx.body = html;
 });
 
 app.use(router.routes()).use(router.allowedMethods());
@@ -36,3 +52,127 @@ app.use(router.routes()).use(router.allowedMethods());
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
+
+async function respond(ctx, jsx) {
+  const clientJSX = await renderJSXToClientJSX(jsx);
+
+  if (ctx.request.query.jsx != null) {
+    const clientJSXString = JSON.stringify(clientJSX, stringifyJSX);
+
+    ctx.type = 'application/json';
+    ctx.body = clientJSXString;
+
+    return;
+  }
+
+
+  const files = await readdir(join(import.meta.dirname, 'app'));
+  const imports = await Promise.all(
+      files.map((file) => import(join(import.meta.dirname, 'app', file)))
+  );
+
+  const modules = imports.reduce((acc, mod, i) => {
+    if (mod.default?.$$typeof === Symbol.for('react.client.reference')) {
+      acc += `
+        import $${i} from './${mod.default.filename}';
+        window.__CLIENT_MODULES__['${mod.default.filename}'] = $${i};
+      `;
+    }
+
+    return acc;
+  }, `window.__CLIENT_MODULES__ = {};`);
+
+  const clientJSXString = JSON.stringify(clientJSX, stringifyJSX);
+
+  const clientHtml = `
+        ${renderToString(
+      await storage.run({ ssr: true }, () => renderJSXToClientJSX(jsx))
+  )}
+
+    <script>
+      window.__INITIAL_CLIENT_JSX_STRING__ = ${JSON.stringify(
+      clientJSXString
+  ).replace(/</g, '\\u003c')};
+    </script>
+    <script type="importmap">
+      {
+        "imports": {
+          "react": "https://esm.sh/react@canary",
+          "react-dom/client": "https://esm.sh/react-dom@canary/client"
+        }
+      }
+    </script>
+    <script type="module">
+      ${modules};
+    </script>
+  <script type="module" src="/client.js"></script>
+`;
+
+  ctx.type = 'text/html';
+  ctx.body = clientHtml;
+}
+
+function stringifyJSX(key, value) {
+  if (value === Symbol.for('react.element')) {
+    return '$RE';
+  } else if (value === Symbol.for('react.client.reference')) {
+    return '$RE_M';
+  } else if (typeof value === 'string' && value.startsWith('$')) {
+    return '$' + value;
+  } else {
+    return value;
+  }
+}
+
+const storage = new AsyncLocalStorage();
+
+async function renderJSXToClientJSX(jsx) {
+  if (
+      typeof jsx === 'string' ||
+      typeof jsx === 'number' ||
+      typeof jsx === 'boolean' ||
+      jsx == null
+  ) {
+    return jsx;
+  } else if (Array.isArray(jsx)) {
+    return Promise.all(jsx.map((child) => renderJSXToClientJSX(child)));
+  } else if (jsx != null && typeof jsx === 'object') {
+    if (jsx.$$typeof === Symbol.for('react.element')) {
+      if (typeof jsx.type === 'string') {
+        return {
+          ...jsx,
+          props: await renderJSXToClientJSX(jsx.props),
+        };
+      } else if (typeof jsx.type === 'function') {
+        const Component = jsx.type;
+        const props = jsx.props;
+        const returnedJsx = await Component(props);
+
+        return renderJSXToClientJSX(returnedJsx);
+      } else if (jsx.type.$$typeof === Symbol.for('react.client.reference')) {
+        const ssr = storage.getStore()?.ssr;
+
+        if (!ssr) {
+          return jsx;
+        }
+
+        const m = await import(`./${jsx.type.filename}?original`);
+
+        return { ...jsx, type: m[jsx.type.name] };
+      } else {
+        throw new Error('Not implemented.');
+      }
+    } else {
+      return Object.fromEntries(
+          await Promise.all(
+              Object.entries(jsx).map(async ([propName, value]) => [
+                propName,
+                await renderJSXToClientJSX(value),
+              ])
+          )
+      );
+    }
+  } else {
+    throw new Error('Not implemented');
+  }
+}
